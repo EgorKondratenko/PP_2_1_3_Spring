d779fdb4b9f8573ff21f5b78c8c46c44c987ac15
EgorKondratenko <egorkondratenko91@gmail.com>
2023-04-01 13:49:29 +0300

    load

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+
+public class Cat {
+
+    private String say;
+
+    public String getValue() {
+        return say;
+    }
+
+    public void setValue(String say) {
+        this.say = say;
+    }
+}
